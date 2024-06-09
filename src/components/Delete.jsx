d0be05afbf5bd7a4e06a5ba42b592c04fb62aea1
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-09 20:21:55 +0530

    values form delete is done

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,35 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const Delete = () => {
+
+  const [data, setData] = useState(
+
+    {
+      "photoId": "",
+    }
+  )
+const inputHandler = (event) => {
+  setData({...data,[event.target.name]:event.target.value})
+}
+const outputHandler = () => {
+  console.log(data)
+}
+
+
   return (
     <div>
-<NavBar/>
-<div className="container">
+      <NavBar />
+      <div className="container">
         <div className="row">
-            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
             <div className="row g-3">
 
-                <h2>Delete</h2>
+              <h2>Delete</h2>
 
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                 <label htmlFor="" className="form-label">Photo Id</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='photoId' value={data.photoId} onChange={inputHandler} />
 
-                </div>
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                <button className="btn btn-danger">Delete</button>
+                <button className="btn btn-danger" onClick={outputHandler}>Delete</button>
 
-                </div>
+              </div>
             </div>
 
 
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
 
 
 
